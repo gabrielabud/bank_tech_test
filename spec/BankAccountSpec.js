6b6cf7951a2e668afcb1b4715e793fa1b285b527
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-02 17:13:45 +0000

    Add test for initialisation of object of type BankAccount

--- spec/BankAccountSpec.js
+++ spec/BankAccountSpec.js
@@ -8,4 +8,8 @@ describe("BankAccount", function() {
   it("has a zero balance", function() {
     expect(account.balance()).toEqual(0)
   });
+
+  it("when initialising is an object of type BankAccount", function() {
+    expect(account).toEqual(jasmine.any(BankAccount));
+  });
 });
