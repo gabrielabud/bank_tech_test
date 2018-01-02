e3efa17da3405d637156b613e1809cc44fdb3690
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-02 13:57:11 +0000

    Add TransactionList prototype

---
+++ src/transactionList.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function(exports) {
+  function TransactionList() {
+    this._tranList = [];
+  };
+
+  TransactionList.prototype.showTransactions = function () {
+    return this._tranList;
+  }
+
+  TransactionList.prototype.saveTransactions = function(transaction) {
+    this._tranList.push(transaction);
+    return this._tranList;
+  };
+
+  exports.TransactionList = TransactionList;
+})(this);
