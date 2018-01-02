01db941e2b9100cc00a27fdbac3573b98a542f0b
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-02 13:24:16 +0000

    Add Transaction Spec

---
+++ spec/TransactionSpec.js
@@ -0,0 +1,20 @@
+describe("Transaction", function() {
+  var a;
+
+  beforeEach(function() {
+    a = new Transaction("02/01/2012","debit",1000)
+  });
+
+  it("has date", function() {
+    expect(a.date()).toEqual("02/01/2012");
+  });
+
+  it("has a type", function() {
+    expect(a.typeTran()).toEqual("debit");
+  });
+
+  it("has an amount", function() {
+    expect(a.amount()).toEqual(1000);
+  });
+
+});
